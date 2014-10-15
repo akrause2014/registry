b86e930394b292ccf5c085b5669f83bf59db1372
Iraklis A. Klampanos <iaklampanos@gmail.com>
2014-10-15 09:51:59 +0300

    added edit/update

--- dispel4py_extension.py
+++ dispel4py_extension.py
@@ -18,6 +18,11 @@ The IPython extension for viewing and listing modules from the Dispel4Py registr
 
 from dispel4py.registry import core, client
 import traceback
+#from __future__ import print_function
+from IPython.html.widgets import interact, interactive, fixed
+from IPython.html import widgets
+from IPython.display import display
+import sys 
 
 def _initRegistry():
     client.config = client.configure()
@@ -38,19 +43,84 @@ def _listObjects(name):
     except core.UnknownPackageException:
         objs = []
     return objs
+    
+def _edit(name):
+    reg = _initRegistry()
+    try:
+        source = reg.get_code(name)
+        w = widgets.TextareaWidget(
+            description=name,
+            value=source
+        )
+        b = widgets.ButtonWidget(description='Update')
+        return (reg,w,b)
+    except Exception as err:
+        import traceback
+        traceback.print_exc()
+        sys.stderr.write("An error occurred:\n%s\n" % err)
+        sys.exit(-1)
+
+# XXX : appears to be necessary evil ... need to think of a workaround
+# name_to_update = None
+# code_to_update = None
+# reg_for_update = None
+# def _update(b):
+#     try:
+#         if name_to_update != None and code_to_update != None and reg_for_update != None:
+#             reg.update_code(name_to_update, code_to_update)
+#         name_to_update = code_to_update = reg_for_update = None
+#     except Exception as err:
+#         import traceback
+#         traceback.print_exc()
+#         sys.stderr.write("An error occurred:\n%s\n" % err)
+#         sys.exit(-1)
 
 from IPython.core.magic import (Magics, magics_class, line_magic)
     
 @magics_class
 class Dispel4PyMagics(Magics):
     '''
-    Creates the dispel4py command in iPython, to be used interactively for training and other purposes.
+    Creates the dispel4py command in iPython, to be used interactively for training and other purposes.  
     '''
 
+    # XXX : appears to be necessary evil ... need to think of a workaround
+    name_to_update = None
+    code_to_update = None
+    reg_for_update = None
+    textarea_for_update = None
+    def _update(self, b):
+        try: 
+            if not self.textarea_for_update is None and not self.textarea_for_update.value is None and not self.name_to_update is None and not self.code_to_update is None and not self.reg_for_update is None:
+                self.reg_for_update.update_code(self.name_to_update, self.textarea_for_update.value)
+            self.name_to_update = self.code_to_update = self.reg_for_update = None
+            print self.textarea_for_update.description, 'updated.'
+        except Exception as err:
+            import traceback
+            traceback.print_exc()
+            sys.stderr.write("An error occurred:\n%s\n" % err)
+        
     @line_magic
     def dispel4py(self, line):
         command = line.split()
-        if command[0] == 'list': 
+        if command[0] == 'edit':
+            if len(command) == 1:
+                print 'Name not provided.'
+            reg, w, b = _edit(command[1])
+            self.textarea_for_update = w
+            display(w)
+            display(b)
+            self.reg_for_update = reg
+            self.name_to_update = w.description
+            self.code_to_update = w.value
+            b.on_click(self._update)
+        elif command[0] == 'register_pe':
+            pass
+        elif command[0] == 'register_fn':
+            pass
+        elif command[0] == 'list' and len(command) == 1:
+            reg = _initRegistry()
+            client.list(reg)        
+        elif command[0] == 'list':
             pkgs = _listPackages(command[1])
             if pkgs: 
                 print "Packages:"
@@ -77,6 +147,20 @@ class Dispel4PyMagics(Magics):
                     print "Resource '%s' not found\n" % name
                 else:
                     print source
+            except Exception as err:
+                print traceback.format_exc()
+                print "An error occurred."
+        elif command[0] == 'workspace':
+            try:
+                reg = _initRegistry()
+                client.workspace(reg)
+            except Exception as err:
+                print traceback.format_exc()
+                print "An error occurred."
+        elif command[0] == 'clone':
+            try:
+                reg = _initRegistry()
+                client.clone_workspace(reg)
             except Exception as err:
                 print traceback.format_exc()
                 print "An error occurred."
